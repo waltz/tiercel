3be5a14d088db641b7ecb884ea971f876ae0f9e9
CJ Bryan <christianbryan@gmail.com>
2016-03-07 11:48:24 -0800

    Log output when backing up the root FS.

--- roles/rootbackup/files/rootbackup.sh
+++ roles/rootbackup/files/rootbackup.sh
@@ -5,5 +5,12 @@ set +xe
 DESTINATION=/mnt/plop
 BACKUP_FILE_NAME=root-fs-backup.tar.gz
 BACKUP_PATH=$DESTINATION/$BACKUP_FILE_NAME
+LOGFILE=/var/log/rootbackup.log
 
-tar cvpzf $BACKUP_PATH --exclude=$BACKUP_PATH --one-file-system /
+tar \
+  cvpzf \
+  $BACKUP_PATH \
+  --exclude=$BACKUP_PATH \
+  --one-file-system \
+  / \
+  | tee --append $LOGFILE
